019becc1c78e083dbb3eb1bc191a5d79c5863dbc
Esteban Alvarado Vargas <estalvgs1999@gmail.com>
2021-06-15 10:02:50 -0600

    [config][fix] password fixed

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,8 +7,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     host: 'mongodb+srv://db_client:tpmdb_client2021@taskplannerce.vdwdf.mongodb.net/taskplannerdb?retryWrites=true&w=majority',
     port: 27017,
     username: 'db_client',
-    password:'tbmdb_client2021',
-    database:  'taskplannerdb',
+    password: 'tpmdb_client2021',
+    database: 'taskplannerdb',
     entities: [
         'dist/**/*.entity.js',
         './**/*.entity.js'],
