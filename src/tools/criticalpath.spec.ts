a39a31c31d235172c4d153bdedec537a3e130719
esteban alvarado <estalvgs1999@gmail.com>
2021-06-24 16:51:07 -0600

    test(CPM): :white_check_mark: Critical Path testing
    Implemented and approved all unit tests related to the critical path module.
    
    #24 #TB07

--- src/tools/criticalpath.spec.ts
+++ src/tools/criticalpath.spec.ts
@@ -1,6 +1,7 @@
 import { CriticalPath } from './criticalpath';
 import { Node } from "./node";
 import { Graph } from "./graph";
+import { TareasI } from '../tareas/tareas.entity';
 
 describe('Nodes test', () => {
   it('should create some nodes', () => {
@@ -33,12 +34,12 @@ describe('Nodes test', () => {
     expect(node.latestStart).toBe(4);
   });
 
-  it('should calculate correctly the float of the activity', () => {
+  it('should calculate correctly the slack of the activity', () => {
     const nodeA: Node = new Node('A','2021-06-21','2021-06-30');
 
     nodeA.setEarliestStart(0);
     nodeA.setLatestFinish(13);
-    nodeA.calculateFloat();
+    nodeA.calculateSlack();
 
     expect(nodeA.isCriticNode()).toBeFalsy();
 
@@ -46,7 +47,7 @@ describe('Nodes test', () => {
     const nodeB: Node = new Node('B','2021-07-01','2021-07-15');
     nodeB.setEarliestStart(0);
     nodeB.setLatestFinish(14);
-    nodeB.calculateFloat();
+    nodeB.calculateSlack();
 
     expect(nodeB.isCriticNode()).toBeTruthy();
   });
@@ -91,8 +92,7 @@ describe('Graph test',() => {
     graph.addEdge('B','E');
     graph.addEdge('C','D');
 
-    console.log(graph.bfs('A'));
-    expect(graph.bfs('A')).toBeDefined();
+    expect(graph.bfs('A')).toStrictEqual([ 'A', 'B', 'C', 'E', 'D' ]);
 
   });
 
@@ -104,5 +104,86 @@ describe('Graph test',() => {
     expect(graph.isLeafNode('A')).toBeFalsy();
     expect(graph.isLeafNode('E')).toBeTruthy();
 
+  });
+
+  it('should add correctly apply BFS to the graph (backwards)', () => {
+
+    const nodeF: Node = new Node('F');
+
+    graph.addNode(nodeF);
+
+    graph.addEdge('E','F');
+    graph.addEdge('D','F');
+
+    expect(graph.bfs('F')).toStrictEqual([ 'F', 'E', 'D', 'B', 'C', 'A' ]);
+
+  });
+  
+});
+
+describe('Critical Path test',() => {
+
+  it('It should correctly calculate the critical path for a given task list', () => {
+
+    // Create some tasks
+    const tasks: TareasI[] = [];
+    const t0: TareasI= new TareasI();
+    const t1: TareasI= new TareasI();
+    const t2: TareasI= new TareasI();
+    const t3: TareasI= new TareasI();
+    const t4: TareasI= new TareasI();
+    const t5: TareasI= new TareasI();
+    const t6: TareasI= new TareasI();
+    const t7: TareasI= new TareasI();
+
+    t0.id = 'A';
+    t0.startDate = '2021-06-01';
+    t0.endDate = '2021-06-07';
+    t0.dependency = [];
+    tasks.push(t0);
+    
+    t1.id = 'B';
+    t1.startDate = '2021-06-01';
+    t1.endDate = '2021-06-05';
+    t1.dependency = [];
+    tasks.push(t1);
+
+    t2.id = 'C';
+    t2.startDate = '2021-06-01';
+    t2.endDate = '2021-06-04';
+    t2.dependency = ['A'];
+    tasks.push(t2);
+
+    t3.id = 'D';
+    t3.startDate = '2021-06-01';
+    t3.endDate = '2021-06-05';
+    t3.dependency = ['B'];
+    tasks.push(t3);
+
+    t4.id = 'E';
+    t4.startDate = '2021-06-01';
+    t4.endDate = '2021-06-04';
+    t4.dependency = ['B'];
+    tasks.push(t4);
+
+    t5.id = 'F';
+    t5.startDate = '2021-06-01';
+    t5.endDate = '2021-06-11';
+    t5.dependency = [];
+    tasks.push(t5);
+
+    t6.id = 'G';
+    t6.startDate = '2021-06-01';
+    t6.endDate = '2021-06-04';
+    t6.dependency = ['E','F'];
+    tasks.push(t6);
+
+    t7.id = 'H';
+    t7.startDate = '2021-06-01';
+    t7.endDate = '2021-06-03';
+    t7.dependency = ['C','D'];
+    tasks.push(t7);
+
+    expect(CriticalPath.getCriticalPath(tasks)).toStrictEqual(['F','G']);
   });
 });
